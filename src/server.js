2e972324023614e927cc5861763cb8a64486994f
Le Khanh Duy <lkduy2602@gmail.com>
2022-09-12 17:54:28 +0700

    Cai dat packet

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 import express from "express";
+import "dotenv/config";
 
 const app = express();
 
-const hostname = "localhost";
-const port = 5000;
+const hostname = process.env.HOSTNAME || "localhost";
+const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello World</h1>");
