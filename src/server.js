f95c1bfdbd2776696cf37a5b33627238b99400f5
Le Khanh Duy <lkduy2602@gmail.com>
2022-09-12 18:04:31 +0700

    test

--- src/server.js
+++ src/server.js
@@ -1,7 +1,21 @@
+import path from "path";
+
 import express from "express";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
 import "dotenv/config";
 
 const app = express();
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, "public")));
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 const hostname = process.env.HOSTNAME || "localhost";
 const port = process.env.PORT || 5000;
@@ -10,6 +24,6 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello World</h1>");
 });
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server is running at http://${hostname}:${port}/`);
 });
